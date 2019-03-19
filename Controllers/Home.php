4a4edda9ba9576551f793d25ab5f7626f1664c7a
Guadalupe1233 <guadalupesilvaalvarado@gmail.com>
2019-03-19 13:30:18 -0600

    jhhhj
    ygygyg

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -2,7 +2,7 @@
 
 require 'Main.php';
 
-include 'Models/Ropa.php';
+include 'Models/hombres.php';
 
 class Home extends Main {
 	
@@ -12,76 +12,77 @@ class Home extends Main {
 
 	public function load() {
 		# ORM Instance
-		$ropa = new Ropa();
+		error_reporting(0);
+		$ropa = new hombres();
 
 		# Test FindAll
-		echo "<br />";
-		echo "<table>";
-		foreach ($ropa->findAll() as $array) {
-			echo "<tr>";
-			foreach ($array as $key => $value) {
-				echo "<td>".$value."</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";
+		#echo "<br />";
+		#echo "<table>";
+		#foreach ($ropa->findAll() as $array) {
+		#	echo "<tr>";
+		#	foreach ($array as $key => $value) {
+		#		echo "<td>".$value."</td>";
+		#	}
+		#	echo "</tr>";
+		#}
+		#echo "</table>";
 
 		# Test Remove
-		if( $ropa->remove(['id' => '1']) ) {
-			echo "<br />Se elimino el registro 1<br />";
-		} else {
-			echo "<br />Error al eliminar<br />";
-		}
+		#if( $ropa->remove(['id' => '1']) ) {
+		#	echo "<br />Se elimino el registro 1<br />";
+		#} else {
+		#	echo "<br />Error al eliminar<br />";
+		#}
 
 		# Test Find
-		echo "<br />";
-		echo "<table>";
-		foreach ($ropa->find([],[]) as $array) {
-			echo "<tr>";
-			foreach ($array as $key => $value) {
-				echo "<td>".$value."</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";
+		#echo "<br />";
+		#echo "<table>";
+		#foreach ($ropa->find([],[]) as $array) {
+		#	echo "<tr>";
+		#	foreach ($array as $key => $value) {
+		#		echo "<td>".$value."</td>";
+		#	}
+		#	echo "</tr>";
+		#}
+		#echo "</table>";
 
 		# Test Add
-		if( $ropa->add(['1','Jeans']) ) {
-			echo "<br />Se agrego un registro<br />";
-		} else {
-			echo "<br />Error al agregar<br />";
-		}
+		#if( $ropa->add(['1','Jeans']) ) {
+		#	echo "<br />Se agrego un registro<br />";
+		#} else {
+		#	echo "<br />Error al agregar<br />";
+		#}
 
 		# Test Find
-		echo "<br />";
-		echo "<table>";
-		foreach ($ropa->find([],['id' => 1]) as $array) {
-			echo "<tr>";
-			foreach ($array as $key => $value) {
-				echo "<td>".$value."</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";
+		#echo "<br />";
+		#echo "<table>";
+		#foreach ($ropa->find([],['id' => 1]) as $array) {
+		#	echo "<tr>";
+		#	foreach ($array as $key => $value) {
+		#		echo "<td>".$value."</td>";
+		#	}
+		#	echo "</tr>";
+		#}
+		#echo "</table>";
 
 		# Test Add
-		if( $ropa->edit(['descripcion' => 'Pantalones'],['id' => '1']) ) {
-			echo "<br />Se modifico el registro 1<br />";
-		} else {
-			echo "<br />Error al modificar<br />";
-		}
+		#if( $ropa->edit(['descripcion' => 'Pantalones'],['id' => '1']) ) {
+		#	echo "<br />Se modifico el registro 1<br />";
+		#} else {
+		#	echo "<br />Error al modificar<br />";
+		#}
 
 		# Test Find
-		echo "<br />";
-		echo "<table>";
-		foreach ($ropa->find(['descripcion'],['id' => 1]) as $array) {
-			echo "<tr>";
-			foreach ($array as $key => $value) {
-				echo "<td>".$value."</td>";
-			}
-			echo "</tr>";
-		}
-		echo "</table>";
+		#echo "<br />";
+		#echo "<table>";
+		#foreach ($ropa->find(['descripcion'],['id' => 1]) as $array) {
+		#	echo "<tr>";
+		#	foreach ($array as $key => $value) {
+		#		echo "<td>".$value."</td>";
+		#	}
+		#	echo "</tr>";
+		#}
+		#echo "</table>";
 
 		$this->loadView('main');
 	}
--- Resources/Views/main.php
+++ Resources/Views/main.php
@@ -1,10 +1,121 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>Principal</title>
-</head>
-<body style="background: linear-gradient(to right, rgba(0,255,0,0), rgba(0,255,200,1));">
-	<h1 style="color: #888;">LaraCode</h1>
-	<p><h4>Bienvenido</h4></p>
-</body>
+<!--
+* OOPS - Perfect 404 pages Pack
+* Build Date: October 2016
+* Last Update: October 2016
+* Author: Madeon08 for ThemeHelite
+* Copyright (C) 2016 ThemeHelite
+* This is a premium product available exclusively here : http://themeforest.net/user/Madeon08/portfolio
+* -->
+<html lang="en-us" class="no-js">
+
+	<head>
+		<meta charset="utf-8">
+		<title>OOPS - Flat 404 Not found</title>
+		<meta name="description" content="The description should optimally be between 150-160 characters.">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="ThemeHelite">
+
+        <!-- ================= Favicons ================== -->
+        <!-- Standard -->
+        <link rel="shortcut icon" href="Resources/Images/favicon.png">
+        <!-- Retina iPad Touch Icon-->
+        <link rel="apple-touch-icon" sizes="144x144" href="Resources/Images/favicon-retina-ipad.png">
+        <!-- Retina iPhone Touch Icon-->
+        <link rel="apple-touch-icon" sizes="114x114" href="Resources/Images/favicon-retina-iphone.png">
+        <!-- Standard iPad Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="72x72" href="Resources/Images/favicon-standard-ipad.png">
+        <!-- Standard iPhone Touch Icon--> 
+        <link rel="apple-touch-icon" sizes="57x57" href="Resources/Images/favicon-standard-iphone.png">
+
+		<!-- ============== Resources style ============== -->
+		<link rel="stylesheet" type="text/css" href="Resources/CSS/style.css" />
+	</head>
+
+	<body class="bubble">
+
+        <canvas id="canvasbg"></canvas>
+        <canvas id="canvas"></canvas>
+
+        <!-- Your logo on the top left -->
+        <a href="#" class="logo-link" title="back home">
+
+            <img src="Resources/Images/logo.png" class="logo" alt="Company's logo" />
+
+        </a>
+
+        <div class="content">
+
+            <div class="content-box">
+
+                <div class="big-content">
+
+                    <!-- Main squares for the content logo in the background -->
+                    <div class="list-square">
+                        <span class="square"></span>
+                        <span class="square"></span>
+                        <span class="square"></span>
+                    </div>
+
+                    <!-- Main lines for the content logo in the background -->
+                    <div class="list-line">
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                        <span class="line"></span>
+                    </div>
+
+                    <!-- The animated searching tool -->
+                    <i class="fa fa-search color" aria-hidden="true"></i>
+
+                    <!-- div clearing the float -->
+                    <div class="clear"></div>
+
+                </div>
+
+                <!-- Your text -->
+                <h1>Oops! Error 404 not found.</h1>
+
+                <p>The page you were looking for doesn't exist.<br>
+                    We think the page may have moved.</p>
+
+            </div>
+
+        </div>
+
+        <footer class="light">
+
+            <ul>
+                <li><a href="#">Home</a></li>
+
+                <li><a href="#">Search</a></li>
+
+                <li><a href="#">Help</a></li>
+
+                <li><a href="#">Trust & Safety</a></li>
+
+                <li><a href="#">Sitemap</a></li>
+
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            </ul>
+
+        </footer>
+
+        <!-- ///////////////////\\\\\\\\\\\\\\\\\\\ -->
+        <!-- ********** jQuery Resources ********** -->
+        <!-- \\\\\\\\\\\\\\\\\\\/////////////////// -->
+
+        <!-- * Libraries jQuery and Bootstrap - Be careful to not remove them * -->
+        <script src="Resources/JS/jquery.min.js"></script>
+        <script src="Resources/JS/bootstrap.min.js"></script>
+
+        <!-- Bubble plugin -->
+        <script src="Resources/JS/bubble.js"></script>
+
+    </body>
+
 </html>
